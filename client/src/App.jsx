ec338e81be00aa335d837e6baa2bae3b1c88a319
dworrels <99518893+dworrels@users.noreply.github.com>
2025-06-09 10:23:35 -0400

    Initialize TailwindCSS

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -28,7 +28,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Monitor</h1>
+      <h1 className='text-red-700'>Monitor</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
